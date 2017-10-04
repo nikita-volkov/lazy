22c81a637de7f62ea9a9b26cfc5ea6591e3cfa42
Nikita Volkov <nikita.y.volkov@mail.ru>
2017-10-04 20:06:57 +0300

    Correct the implementation, introducing Applicative and Monad

--- library/Lazy.hs
+++ library/Lazy.hs
@@ -9,27 +9,50 @@ where
 import Lazy.Prelude hiding (lazy)
 
 
-data Lazy value = forall x. Lazy !(IORef (LazyState x)) !(x -> value)
+newtype Lazy value = Lazy (IO value)
 
 instance Functor Lazy where
   {-# INLINE fmap #-}
-  fmap mapping (Lazy ref proj) =
-    Lazy ref (mapping . proj)
+  fmap mapping (Lazy io) =
+    Lazy (fmap mapping io)
+
+instance Applicative Lazy where
+  pure = lazy
+  {-# INLINE (<*>) #-}
+  (<*>) (Lazy leftIO) (Lazy rightIO) =
+    Lazy (leftIO <*> rightIO)
+
+instance Monad Lazy where
+  return = pure
+  {-# INLINE (>>=) #-}
+  (>>=) (Lazy leftIO) rightK =
+    Lazy $ do
+      leftValue <- leftIO
+      case rightK leftValue of
+        Lazy rightIO -> rightIO
 
 data LazyState value = UnevaluatedLazyState value | EvaluatedLazyState !value
+
 
 {-# INLINE lazy #-}
 lazy :: value -> Lazy value
 lazy value =
-  Lazy (unsafeDupablePerformIO (newIORef (UnevaluatedLazyState value))) id
+  unsafeDupablePerformIO $ do
+    stateRef <- newIORef (UnevaluatedLazyState value)
+    return $ Lazy $ do
+      state <- readIORef stateRef
+      case state of
+        EvaluatedLazyState !value -> return value
+        UnevaluatedLazyState !value -> do
+          writeIORef stateRef (EvaluatedLazyState value)
+          return value
 
+{-# INLINE unlazy #-}
 unlazy :: Lazy value -> value
-unlazy (Lazy ref proj) =
+unlazy (Lazy io) =
   -- Synchronisation is not an issue here,
   -- since there's nothing dangerous in two threads
   -- occasionally computing the same result.
   -- That would be the price of not having 
   -- to pay for locks-maintenance overhead.
-  unsafeDupablePerformIO $ readIORef ref >>= \case
-    EvaluatedLazyState !value -> return (proj value)
-    UnevaluatedLazyState !value -> writeIORef ref (EvaluatedLazyState value) $> proj value
+  unsafeDupablePerformIO io
