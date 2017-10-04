ace15d6a7cce497d4653ef6120225a12e7d9e370
Nikita Volkov <nikita.y.volkov@mail.ru>
2017-10-04 19:56:52 +0300

    Functor

--- library/Lazy.hs
+++ library/Lazy.hs
@@ -6,24 +6,30 @@ module Lazy
 )
 where
 
-import Lazy.Prelude
+import Lazy.Prelude hiding (lazy)
 
 
-newtype Lazy value = Lazy (IORef (LazyState value))
+data Lazy value = forall x. Lazy !(IORef (LazyState x)) !(x -> value)
+
+instance Functor Lazy where
+  {-# INLINE fmap #-}
+  fmap mapping (Lazy ref proj) =
+    Lazy ref (mapping . proj)
 
 data LazyState value = UnevaluatedLazyState value | EvaluatedLazyState !value
 
+{-# INLINE lazy #-}
 lazy :: value -> Lazy value
 lazy value =
-  Lazy (unsafeDupablePerformIO (newIORef (UnevaluatedLazyState value)))
+  Lazy (unsafeDupablePerformIO (newIORef (UnevaluatedLazyState value))) id
 
 unlazy :: Lazy value -> value
-unlazy (Lazy ref) =
+unlazy (Lazy ref proj) =
   -- Synchronisation is not an issue here,
   -- since there's nothing dangerous in two threads
   -- occasionally computing the same result.
   -- That would be the price of not having 
   -- to pay for locks-maintenance overhead.
   unsafeDupablePerformIO $ readIORef ref >>= \case
-    EvaluatedLazyState !value -> return value
-    UnevaluatedLazyState !value -> writeIORef ref (EvaluatedLazyState value) $> value
+    EvaluatedLazyState !value -> return (proj value)
+    UnevaluatedLazyState !value -> writeIORef ref (EvaluatedLazyState value) $> proj value
